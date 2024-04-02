c7b86afe254aee35666d4cd7abe7a92dcc423041
Min Soe Moe <minsoemoe999@gmail.com>
2024-04-02 22:39:10 +0630

    Found the solution for Webpack 5 not polyfilling
    There are errors like these,
    add a fallback 'resolve.fallback: { "assert": require.resolve("assert/") }'
    - install 'assert
    
    What that means is, I have to install the library called assert by `npm install assert` and then wrote according code in the webpack.config.js. What to write in the webpack.config.js can be seen in the committed webpack.config.js.
    
    Currently not finished, installing libraries.
    
    I think there is something I can do to automate installing those necessary libraries, currently I'm installing those libraries manually 💀

--- Puppeteer Tutorial/webpack.config.js
+++ Puppeteer Tutorial/webpack.config.js
@@ -1,9 +1,33 @@
+// module.exports = {
+//   mode: "production",
+//   entry: "./src/index.js",
+//   output: {
+//     filename: "main.js",
+//     path: __dirname + "/dist",
+//   },
+//   plugins: [new NodePolyfillPlugin()],
+// };
+const path = require("path");
 module.exports = {
-  mode: "production",
+  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
-    path: __dirname + "/dist",
+    path: path.resolve(__dirname, "dist"),
   },
-  plugins: [new NodePolyfillPlugin()],
+  resolve: {
+    fallback: {
+      path: require.resolve("path-browserify"),
+      http: require.resolve("stream-http"),
+      url: require.resolve("url"),
+      crypto: require.resolve("crypto-browserify"),
+      os: require.resolve("os-browserify/browser"),
+      https: require.resolve("https-browserify"),
+      stream: require.resolve("stream-browserify"),
+      zlib: require.resolve("browserify-zlib"),
+      util: require.resolve("util/"),
+
+      // Add more polyfills as needed for other modules
+    },
+  },
 };
