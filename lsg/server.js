1cfb6c32b5fbe2ecd353aa85f0cb67b61b4b0ffd
Min Soe Moe <minsoemoe999@gmail.com>
2024-04-05 19:20:41 +0630

    Successfully scrape data and sending
    creating image using the sent data

--- lsg/server.js
+++ lsg/server.js
@@ -1,27 +1,101 @@
 "use strict";
 const express = require("express");
 const app = express();
+const puppeteer = require("puppeteer");
+
 app.use(express.static("public"));
 app.use(express.static("src"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.set("view engine", "ejs");
-// import { run } from "scrape.js";
-const { run } = require("./scrape.js");
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.post("/scrape", async (req, res) => {
-  console.log(req.body.review_link);
-  res.send("URL sent");
+app.post("/result", async (req, res) => {
+  const reveiwLink = req.body.review_link;
+
   try {
-    let scrapedData = await run(req.body.review_link);
-    console.log(scrapedData);
+    const browser = await puppeteer.launch({
+      // headless: false,
+      defaultViewport: false,
+      userDataDir: "./tmp",
+    });
+
+    const page = await browser.newPage();
+    await page.goto(`${reveiwLink}`, {
+      waitUntil: "load",
+    });
+
+    async function getTextContent(page, selector) {
+      const text = await page.evaluate((sel) => {
+        const element = document.querySelector(sel);
+        return element ? element.textContent.trim() : null;
+      }, selector);
+      return text;
+    }
+
+    const hasSpoilers = async (page) => {
+      const hasSpoiler = await page.evaluate(
+        () =>
+          document.querySelector(`div.review>div.contains-spoilers`) !== null
+      );
+      return hasSpoiler;
+    };
+
+    const hasSpoiler = await hasSpoilers(page);
+
+    const review = await getTextContent(page, `div.review div > h3 ~ div > p`);
+
+    const reviewerName = await getTextContent(page, `span[itemprop='name']`);
+
+    const movieName = await getTextContent(page, `span.film-title-wrapper>a`);
+
+    const movieYear = await getTextContent(
+      page,
+      `span.film-title-wrapper>small>a`
+    );
+
+    const rating = await getTextContent(page, `span.rating.rating-large`);
+
+    let watchedDate = await getTextContent(page, `p.view-date.date-links`);
+    watchedDate = watchedDate.replace(/\s+/g, " ");
+
+    const likes = await getTextContent(page, `p.like-link-target`);
+
+    await browser.close();
+
+    res.render("result", {
+      data: {
+        movieName,
+        reviewerName,
+        review,
+        movieYear,
+        rating,
+        watchedDate,
+        likes,
+        hasSpoiler,
+      },
+    });
+
+    // res.json({
+    //   movieName,
+    //   reviewerName,
+    //   review,
+    //   movieYear,
+    //   rating,
+    //   watchedDate,
+    //   likes,
+    //   hasSpoiler,
+    // });
   } catch (err) {
-    console.error("Error:", error);
-    res.status(500).json({ error: "An error occurred during web scraping" });
+    console.error(err);
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching the review" });
   }
 });
 
--- lsg/src/index.js
+++ lsg/src/index.js
@@ -1,5 +1,4 @@
 "use strict";
-alert("Hello world!");
 console.log("Hello world!");
 document.querySelector("button").addEventListener("click", () => {
   console.log(`${document.querySelector("input").value}`);
